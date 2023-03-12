b0e73e7c54b2bc74a8a69154c740e39bafa484a7
Aleksandr <anpilov.aleksandr@mail.ru>
2023-03-12 11:20:46 +0300

    lesson_6

---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+string convert(int num_1)
+{
+    string rezult = "";
+    while (num_1 > 0)
+    {
+        rezult = num_1 % 2 + rezult;
+        num_1 = num_1 / 2;
+    }
+    return rezult;
+    }
+Console.WriteLine (convert(12));
+//
+
+string Binary(int num)
+{
+    string res = "";
+
+    while (num > 0)
+    {
+        res = num % 2 + res;
+        num /= 2;
+    }
+    return res;
+}
+
+// 12 - 1100, 4 - 100, 45 - 101101
+int n = int.Parse(Console.ReadLine()!);
+string result = Binary(n);
+Console.WriteLine(result);
